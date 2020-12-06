419ba6f3fddf2bb89086c5d18be743ee9d42478b
Wentao Li <lwt1406@buaa.edu.cn>
2020-12-06 16:49:27 +0800

    apply validation procedure

--- wattnet_predict.py
+++ wattnet_predict.py
@@ -2,11 +2,13 @@ import numpy as np
 import torch
 import time
 from torch import optim
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset
+from sklearn.model_selection import train_test_split
 from utils.config import Config
 from models.wattnet import WATTNet
 from utils.metric import RMSELoss
 from utils.background_loader import BackGroundLoader
+from utils.draw_pic import train_process_pic
 
 
 conf = Config()
@@ -35,27 +37,30 @@ para_save_path = conf.get_config('model-paras', 'local' if conf.get_config('run-
 
 def wattnet_predict(x_train, y_train, x_test):
     model = WATTNet(in_dim=sensor_num, out_dim=future_len).to(device)
-    data_loader, x_test = get_dataloader(x_train, y_train, x_test)
-    model = train_model(model, data_loader)
+    train_loader, val_loader, x_test = get_dataloader(x_train, y_train, x_test)
+    model = train_model(model, train_loader, val_loader, draw_loss_pic=True)
     pred = model(x_test)
     return pred.data.to('cpu').numpy()
 
 
-def train_model(model, data_loader):
+def train_model(model, train_loader, val_loader, draw_loss_pic=False):
     rmse = RMSELoss()
-    opt = optim.Adam(model.parameters(), lr=learning_rate)
+    opt = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.0003)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.5, patience=2, threshold=1e-3, min_lr=1e-6)
     min_loss = np.inf
     min_epoch = 0
+    train_loss_record, val_loss_record = [], []
 
     for epoch in range(epoch_num):
 
         now_time = time.strftime('%H:%M:%S')
-        print(f'[{now_time} epoch: {epoch}, lr:{learning_rate}]', end=' ')
+        now_lr = opt.state_dict()['param_groups'][0]['lr']
+        print(f'[{now_time} epoch: {epoch}, lr:{now_lr}]', end=' ')
         model.train()
 
+        # 训练阶段
         train_loss = 0.0
-        for i, data in enumerate(data_loader):
+        for i, data in enumerate(train_loader):
             x, y = data
 
             with torch.set_grad_enabled(True):
@@ -67,13 +72,25 @@ def train_model(model, data_loader):
                 loss.backward()
                 opt.step()
 
-        train_loss /= len(data_loader.dataset)
+        train_loss /= len(train_loader.dataset)
+        train_loss_record.append(train_loss)
         if train_loss < min_loss:
             min_loss = train_loss
             min_epoch = epoch
-        print(f'loss: {train_loss}, min_loss: {min_loss}, min_epoch: {min_epoch}')
 
-        scheduler.step(loss)  # 更新学习率
+        # 验证阶段
+        val_loss = 0.0
+        with torch.no_grad():
+            for i, (x, y) in enumerate(val_loader):
+                val_loss += rmse(model(x), y).item() * len(x)
+        val_loss /= len(val_loader.dataset)
+        val_loss_record.append(val_loss)
+        scheduler.step(val_loss)  # 更新学习率
+        print(f'train_loss: {train_loss}, valid_loss: {val_loss}, min_loss: {min_loss}, min_epoch: {min_epoch}')
+
+    # 绘制 loss 变化图
+    if draw_loss_pic:
+        train_process_pic(train_loss_record, val_loss_record, title='Train Process')
 
     if save_model:  # 保存模型
         col = conf.get_config('predict-col')
@@ -85,13 +102,18 @@ def train_model(model, data_loader):
 
 
 def get_dataloader(x_train, y_train, x_test):
+    # 划分训练集和验证集
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.125)
     # 转换成 tensor
     x_train = torch.from_numpy(x_train).float().to(device)
     y_train = torch.from_numpy(y_train).float().to(device)
+    x_val = torch.from_numpy(x_val).float().to(device)
+    y_val = torch.from_numpy(y_val).float().to(device)
     x_test = torch.from_numpy(x_test).float().to(device)
 
     # 构建 DataSet 和 DataLoader
-    dataset = TensorDataset(x_train, y_train)
-    # data_loader = DataLoader(dataset=dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
-    data_loader = BackGroundLoader(dataset=dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
-    return data_loader, x_test
+    train_dataset = TensorDataset(x_train, y_train)
+    train_loader = BackGroundLoader(dataset=train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+    val_dataset = TensorDataset(x_val, y_val)
+    val_loader = BackGroundLoader(dataset=val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+    return train_loader, val_loader, x_test
