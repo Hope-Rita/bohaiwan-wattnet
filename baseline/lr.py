654febdd88e4ded752d17e336c7de3171d84731a
Wentao Li <lwt1406@buaa.edu.cn>
2021-01-08 13:58:58 +0800

    lr_mul_pred

--- baseline/lr.py
+++ baseline/lr.py
@@ -1,8 +1,23 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
+from utils.config import Config
+
+
+conf = Config()
 
 
 def lr_predict(x_train, y_train, x_test):
     return model_fit(x_train, y_train).predict(x_test)
+
+
+def lr_multiple_predict(x_train, y_train, x_test):
+    m, p, s = x_train.shape
+    x_train = x_train.reshape(m, p * s)
+    y_train = np.mean(y_train, axis=1)
+    linear_reg = model_fit(x_train, y_train)
+    pred = predict(linear_reg, x_test.reshape(len(x_test), p * s))
+    future_len = conf.get_config('data-parameters', 'future-len')
+    return np.expand_dims(pred, 1).repeat(future_len, axis=1)
 
 
 def predict(model, x_test):
