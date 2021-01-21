48f3283a7c54ebd3494ae0248f82054ef45af5d0
Wentao Li <lwt1406@buaa.edu.cn>
2021-01-21 16:33:25 +0800

    lr and mlp: use pca

--- baseline/lr.py
+++ baseline/lr.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
 from utils.config import Config
 
 
@@ -30,6 +31,28 @@ def lr_multiple_predict(x_train, y_train, x_test):
     y_train = y_train.reshape(m, future_len * s)
     linear_reg = model_fit(x_train, y_train)
     pred = predict(linear_reg, x_test.reshape(len(x_test), p * s))
+    return pred.reshape(-1, future_len, s)
+
+
+def lr_pca_predict(x_train, y_train, x_test):
+    """
+    :param x_train: m, pred_len, sensor_num
+    :param y_train: m, future_len, sensor_num
+    :param x_test: m_test, pred_len, sensor_num
+    :return: pred in shape(m_test, future_len, sensor_num)
+    """
+    m, p, s = x_train.shape
+    future_len = conf.get_config('data-parameters', 'future-len')
+    x_train = x_train.reshape(m, p * s)
+    x_test = x_test.reshape(len(x_test), p * s)
+    y_train = y_train.reshape(m, future_len * s)
+
+    estimator = PCA(n_components=100)
+    x_train_pca = estimator.fit_transform(x_train)
+    x_test_pca = estimator.transform(x_test)
+
+    linear_reg = model_fit(x_train_pca, y_train)
+    pred = predict(linear_reg, x_test_pca)
     return pred.reshape(-1, future_len, s)
 
 
--- single_predict.py
+++ single_predict.py
@@ -1,3 +1,5 @@
+import numpy as np
+import time
 from utils.config import Config
 
 config_path = 'config.json'
@@ -16,18 +18,26 @@ from baseline.recurrent import rnn_single_predict, gru_single_predict, lstm_sing
 if __name__ == '__main__':
     pred_target_filename = conf.predict_target
     sensor_name = conf.get_config('data-parameters', 'valid-sensors')
-    pred_func = rnn_single_predict
+    pred_func = lr_single_predict
+    exp_mode = 1
 
     x_train, y_train, x_test, y_test, normal_y = get_data(pred_target_filename, 'nanjing', valid_set=False)
 
     m = []
+    pred_merge = np.zeros(y_test.shape)
     for i in range(len(sensor_name)):
         pred = pred_func(x_train=x_train[:, :, i], y_train=y_train[:, :, i], x_test=x_test[:, :, i])
         pred = normal_y[i].inverse_transform(pred)
+        pred_merge[:, :, i] = pred
         y_test[:, :, i] = normal_y[i].inverse_transform(y_test[:, :, i])
         metrics = metric.all_metric(y_test[:, :, i].reshape(-1), pred.reshape(-1))
         m.append(metrics)
         print(f'sensor-{sensor_name[i]}: {metrics}')
     data_process.average_metric(m, data_process.avg)
-
+    data_process.dump_pred_res(dirname=conf.get_config('predict-res-table', conf.run_location),
+                               filename=time.strftime('%Y-%m-%d-%H-%M-%S') + '_' + pred_func.__name__ + '.csv',
+                               y=y_test,
+                               pred=pred_merge,
+                               sensor_name=sensor_name
+                               )
     print('Process end.')
