3299447e6f44c0eea364c0da4d736a60fa2541ba
zt <zoutao@buaa.edu.cn>
2021-09-13 08:28:54 +0800

    36预测15
    {'RMSE': [15.966369, 15.2956915, 15.853934, 15.595096, 15.337424, 15.305303, 15.19241, 15.780254, 15.39623, 15.496757, 15.885438, 16.48805, 16.387793, 16.594416, 16.88303], 'MAE': [12.780245, 12.238621, 12.631033, 12.428151, 12.21814, 12.164382, 11.9293375, 12.49082, 12.143679, 12.192175, 12.485197, 13.04443, 12.915955, 13.027105, 13.324303], 'MAPE': [0.112261064, 0.106160074, 0.11020246, 0.10803837, 0.105860025, 0.10606343, 0.10487439, 0.10863785, 0.106016904, 0.10640025, 0.10912663, 0.11173092, 0.11189264, 0.11282395, 0.113910705], 'PCC': [0.9435708147067826, 0.9492530892342196, 0.9443765501257614, 0.9473088191026338, 0.9456910451258571, 0.9474770203707947, 0.9440246655733753, 0.9390981702004445, 0.9424945102684763, 0.9398013667681175, 0.9371580800768347, 0.9340899720080575, 0.9333946720114366, 0.9293220586438193, 0.9277348681148805]}

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -60,13 +60,13 @@ class Data_utility():
         return [X, Y]
 
 def get_train_data(from_path, to_path):
-    d = Data_utility(data_path=from_path, train=0.7, valid=0.1, window=33, target=15, gap=0)
+    d = Data_utility(data_path=from_path, train=0.7, valid=0.1, window=36, target=15, gap=0)
     train = np.concatenate(d.train, axis=1)
     valid = np.concatenate(d.valid, axis=1)
     test = np.concatenate(d.test, axis=1)
-    np.save(os.path.join(to_path, 'train-33-15.npy'), train[:,:,:7])
-    np.save(os.path.join(to_path, 'val-33-15.npy'), valid[:,:,:7])
-    np.save(os.path.join(to_path, 'test-33-15.npy'), test[:,:,:7])
+    np.save(os.path.join(to_path, 'train-36-15.npy'), train[:,:,:7])
+    np.save(os.path.join(to_path, 'val-36-15.npy'), valid[:,:,:7])
+    np.save(os.path.join(to_path, 'test-36-15.npy'), test[:,:,:7])
     np.save(os.path.join(to_path, 'mean.npy'), d.mean[:4])
     np.save(os.path.join(to_path, 'std.npy'), d.std[:4])
     np.save(os.path.join(to_path, 'max.npy'), d.max[:4])
@@ -86,5 +86,5 @@ def xlsx_to_csv_pd():
 
 if __name__ == '__main__':
     # xlsx_to_csv_pd()
-    get_train_data(from_path='../data/data.csv', to_path='../data/33-15/')
+    get_train_data(from_path='../data/data.csv', to_path='../data/36-15/')
     # print("data_generator")
